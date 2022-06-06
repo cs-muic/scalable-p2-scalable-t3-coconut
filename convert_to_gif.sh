cf23fee5005ff25bd66408b6074a73d2ab3a42ef
tnapat2000 <tnapat2000@gmail.com>
2022-06-07 00:46:05 +0700

    added work queue

--- convert_to_gif.sh
+++ convert_to_gif.sh
@@ -8,4 +8,3 @@
 
 
 ffmpeg -ss 30 -t 10 -i $1 -vf "fps=30,scale=580:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 $2
-# ffmpeg -i $1 -vf "fps=10,scale=320:-1:flags=lanczos" -c:v pam -f image2pipe - | convert -delay 10 - -loop 0 -layers optimize $2
