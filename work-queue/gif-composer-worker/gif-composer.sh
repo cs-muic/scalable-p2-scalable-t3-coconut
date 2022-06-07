23094d9cb4a3ee1594a7795371bcf3b641bb64bc
pearnatnicha <pearpatt2001@gmail.com>
2022-06-07 15:42:53 +0700

    done extract-frames & gif-composer

--- work-queue/gif-composer-worker/gif-composer.sh
+++ work-queue/gif-composer-worker/gif-composer.sh
@@ -4,4 +4,8 @@ START_AT_SECOND=0;
 # echo "Output the GIF using the palette:"
 # ffmpeg -ss $START_AT_SECOND -t 15 -i $1 -loop coconut.gif
 # ffmpeg -framerate 1 -i simpimgs%03d.jpg -loop -1 simpson.gif
-ffmpeg -ss $START_AT_SECOND -t 15 -i palette.jpg output.gif
+# ffmpeg -ss $START_AT_SECOND -t 15 -i palette.jpg output.gif
+
+# convert -delay 2 -loop 0 palette.png -scale 480x360 $1
+# ffmpeg -ss $START_AT_SECOND -t 15 -i $1 -i palette.png -filter_complex "fps=10,scale=320:-1:flags=lanczos[x];[x][1:v]paletteuse" $2
+convert -delay 10 -loop 0 frames/*.png $1
