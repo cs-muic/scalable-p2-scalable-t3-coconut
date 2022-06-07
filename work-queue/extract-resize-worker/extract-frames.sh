0f6ebe47be917c7aa3316397e4a9c06c160801bd
tnapat2000 <tnapat2000@gmail.com>
2022-06-08 03:31:52 +0700

    added rq workqueue

--- work-queue/extract-resize-worker/extract-frames.sh
+++ work-queue/extract-resize-worker/extract-frames.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 START_AT_SECOND=0; # in seconds, if you want to skip the first 30 seconds put 30 here
 
-# echo "Generate a palette:"
-# ffmpeg -y -ss $START_AT_SECOND -t 15 -i $1 -vf fps=30,scale=480:360:-1:flags=lanczos,palettegen palette.png
 ffmpeg -y -ss $START_AT_SECOND -t 15 -i $1 -r 5 -s 480x360 -f image2 frames/frame-%03d.png
