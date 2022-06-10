83401dffe9107c115b9d358459e9319f643c45db
pearnatnicha <pearpatt2001@gmail.com>
2022-06-11 02:07:25 +0700

    download files from minio

--- workqueue/queue-extract/app/functions.py
+++ workqueue/queue-extract/app/functions.py
@@ -6,6 +6,10 @@ import subprocess
 
 from rq import get_current_job
 
+from minio import Minio
+
+
+
 
 def some_long_function(some_input):
     """An example function for redis queue."""
@@ -21,15 +25,22 @@ def some_long_function(some_input):
         "result": some_input,
     }
 
-def execute_extract(input):
+def execute_extract(vdo_input):
     job = get_current_job()
     pwd = os.getcwd()
-    video = input.get('video')
-    subprocess.Popen([f'{pwd}/app/extract-frames.sh', f'{pwd}/app/{video}'])
+    video = vdo_input.get('video')
+    client = Minio(
+        "127.0.0.1:7000",
+        access_key="minio",
+        secret_key="minio123",
+        secure=False
+    )
+    client.fget_object("video", str(video), f'{pwd}/input_vdo/{video}')
+    subprocess.Popen([f'{pwd}/app/extract-frames.sh', f'{pwd}/input_vdo/{video}'])
     return {
         "job_id": job.id,
         "job_enqueued_at": job.enqueued_at.isoformat(),
         "job_started_at": job.started_at.isoformat(),
-        "input": input,
-        "result": input,
+        "input": vdo_input,
+        "result": vdo_input,
     }
